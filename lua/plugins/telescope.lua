b02ece4528410f5d5c81336b042f7c9ad71ef4e7
JackFromSchool <jack.p.saenz@gmail.com>
2025-05-07 16:14:31 -0500

    Small Change

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,16 +1,18 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release ; cmake --build build --config Release",
-		},
-	},
+   {
+      "nvim-telescope/telescope.nvim",
+      branch = "0.1.x",
+      dependencies = {
+         "nvim-lua/plenary.nvim",
+         {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release ; cmake --build build --config Release",
+         },
+      },
 
-	keys = {
-		{ "<leader>ff", require("telescope.builtin").find_files, desc = "Telescope: Find Files" },
-		{ "<leader>fg", require("telescope.builtin").live_grep, desc = "Telescope: Live Grep" },
-	},
+      keys = {
+         { "<leader>ff", require("telescope.builtin").find_files, desc = "Telescope: Find Files" },
+         { "<leader>fg", require("telescope.builtin").live_grep,  desc = "Telescope: Live Grep" },
+      },
+   },
 }
