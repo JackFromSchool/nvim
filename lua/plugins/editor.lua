68b06f24cb3328741ebdee141f7b51d38fc10ab4
JackFromSchool <jack.p.saenz@gmail.com>
2025-05-11 15:33:31 -0500

    Small Changes

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,67 +1,68 @@
 return {
-	-- Autopairs
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-	},
+   -- Autopairs
+   {
+      "windwp/nvim-autopairs",
+      event = "InsertEnter",
+      config = true,
+   },
 
-	-- Better Movement
-	{
-		"ggandor/leap.nvim",
-		init = function()
-			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
-			vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
-		end,
-	},
+   -- Better Movement
+   {
+      "ggandor/leap.nvim",
+      init = function()
+         vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward)")
+         vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward)")
+      end,
+   },
 
-	-- f Search Style
-	{
-		"jinh0/eyeliner.nvim",
-		config = function()
-			require("eyeliner").setup({
-				highlight_on_key = true,
-				dim = true,
-			})
-		end,
-	},
+   -- f Search Style
+   {
+      "jinh0/eyeliner.nvim",
+      config = function()
+         require("eyeliner").setup({
+            highlight_on_key = true,
+            dim = true,
+         })
+      end,
+   },
 
-	-- Comment Tools
-	{
-		"numToStr/Comment.nvim",
-		opts = {},
-	},
+   -- Comment Tools
+   {
+      "numToStr/Comment.nvim",
+      opts = {},
+   },
 
-	-- NOTE: Nice TODO's
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{ "<leader>ft", ":TodoTelescope<CR>", desc = "Open TODO Telescop" },
-		},
-	},
+   -- NOTE: Nice TODO's
+   {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      event = "VeryLazy",
+      opts = {},
+      keys = {
+         { "<leader>ft", ":TodoTelescope<CR>", desc = "Open TODO Telescop" },
+      },
+   },
 
-	-- Git Blame
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
+   -- Git Blame
+   {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+         require("gitsigns").setup()
+      end,
+   },
 
-	-- Better Quickfix
-	{
-		"stevearc/quicker.nvim",
-		event = "FileType qf",
-		opts = {},
-	},
+   -- Better Quickfix
+   {
+      "stevearc/quicker.nvim",
+      event = "FileType qf",
+      opts = {},
+   },
 
-	-- Easier Marks
-	{
-		"chentoast/marks.nvim",
-		event = "VeryLazy",
-		opts = {},
-	},
+   -- Easier Marks
+   {
+      "chentoast/marks.nvim",
+      event = "VeryLazy",
+      opts = {},
+   },
+
 }
