472655b17b3ee815dd89dd78909f6adcd6066502
JackFromSchool <jack.p.saenz@gmail.com>
2025-05-12 17:41:37 -0500

    Added Multigrep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,10 +9,23 @@ return {
             build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release ; cmake --build build --config Release",
          },
       },
+      config = function()
+         require('telescope').setup {
+            pickers = {
+               find_files = {
+                  theme = "ivy"
+               },
+               buffers = {
+                  theme = "ivy"
+               }
+            },
+         }
+      end,
 
       keys = {
-         { "<leader>ff", require("telescope.builtin").find_files, desc = "Telescope: Find Files" },
-         { "<leader>fg", require("telescope.builtin").live_grep,  desc = "Telescope: Live Grep" },
+         { "<leader>ff", require("telescope.builtin").find_files,    desc = "Telescope: Find Files" },
+         { "<leader>fg", require("config.telescope").live_multigrep, desc = "Telescope: Live Grep" },
+         { "<leader>fb", require("telescope.builtin").buffers,       desc = "Telescope: Find Buffers" },
       },
    },
 }
